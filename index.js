8c74b36e0d484e1a25fdb6c47348d01eceb9465f
Sadman Saadat <sadmansaadat99@gmail.com>
2021-08-09 11:19:35 +0600

    complete the website using express, node

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 // index.js
 
 /**
- * Required External Modules
+ * Required External Modules------------------------------------------------------------------------------
  */
 
-/**
- * App Variables
- */
+const express = require("express");
+const path = require("path");  //The path module provides you with utilities for working with file and directory paths
 
 /**
- *  App Configuration
+ * App Variables------------------------------------------------------------------------------------------
  */
+const app = express();
+const port = process.env.PORT || "8000";
+/**
+ *  App Configuration-------------------------------------------------------------------------------------
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+/**
+ * Routes Definitions------------------------------------------------------------------------------------
+ */
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+})
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
 
 /**
- * Routes Definitions
+ * Server Activation-------------------------------------------------------------------------------------
  */
-
-/**
- * Server Activation
- */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+})
